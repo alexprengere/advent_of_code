6fd0a33b6c46dd58ca70e20a056dcb582651f860
Alex <alexprengere@gmail.com>
2020-12-24 10:54:19 +0100

    Optimize day 22

--- 2020/22/python/main.py
+++ 2020/22/python/main.py
@@ -13,22 +13,22 @@ for row in sys.stdin:
 
 
 def play(deck_1, deck_2, recursive):
-    deck_1 = deck_1.copy()
-    deck_2 = deck_2.copy()
-
+    # Returns True if player 1 wins, False if player 2 wins.
+    # Decks will be modified in place.
     seen = set()
+
     while deck_1 and deck_2:
         if recursive:
             state = tuple(deck_1), tuple(deck_2)
             if state in seen:
-                return True, deck_1
+                return True  # player 1 wins
             seen.add(state)
 
         card_1 = deck_1.popleft()
         card_2 = deck_2.popleft()
 
         if len(deck_1) >= card_1 and len(deck_2) >= card_2 and recursive:
-            player_1_wins, _ = play(
+            player_1_wins = play(
                 deque(islice(deck_1, None, card_1)),
                 deque(islice(deck_2, None, card_2)),
                 recursive,
@@ -41,16 +41,18 @@ def play(deck_1, deck_2, recursive):
         else:
             deck_2.extend([card_2, card_1])
 
-    return (True, deck_1) if deck_1 else (False, deck_2)
+    return True if deck_1 else False
 
 
-# PART 1
+# PART 1: recursive = False
+# PART 2: recursive = True
 #
-_, deck_w = play(decks["1"], decks["2"], recursive=False)
-print(sum(card * cn for cn, card in enumerate(reversed(deck_w), start=1)))
-
-
-# PART 2
-#
-_, deck_w = play(decks["1"], decks["2"], recursive=True)
-print(sum(card * cn for cn, card in enumerate(reversed(deck_w), start=1)))
+for recursive in False, True:
+    # We copy the decks for next parts, because "play" modifies them
+    deck_1 = decks["1"].copy()
+    deck_2 = decks["2"].copy()
+    if play(deck_1, deck_2, recursive):
+        deck_w = deck_1
+    else:
+        deck_w = deck_2
+    print(sum(card * cn for cn, card in enumerate(reversed(deck_w), start=1)))
