3ae2f57e82f7be0e83cc9fce1e5e132e109eebe1
Alex <alexprengere@gmail.com>
2020-12-27 11:00:54 +0100

    Optimize day 15

--- 2020/15/python/main.py
+++ 2020/15/python/main.py
@@ -1,5 +1,20 @@
 import sys
 
+
+def memory_game(numbers, turns):
+    last_turns = {n: turn for turn, n in enumerate(numbers[:-1], start=1)}
+    last_spoken = numbers[-1]
+
+    for turn_1 in range(len(numbers), turns):
+        # turn_1 is "turn - 1": the last turn where last_spoken was spoken
+        # last_turns[last_spoken] is the turn before that (not updated yet to "turn - 1").
+        last_turn = last_turns.get(last_spoken, turn_1)
+        last_turns[last_spoken] = turn_1
+        last_spoken = turn_1 - last_turn
+
+    return last_spoken
+
+
 numbers = []
 for n in next(sys.stdin).split(","):
     numbers.append(int(n))
@@ -8,19 +23,5 @@ for n in next(sys.stdin).split(","):
 # PART 1: 2020
 # PART 2: 30_000_000
 #
-TURNS = 30_000_000
-
-last_turns = {n: turn for turn, n in enumerate(numbers[:-1], start=1)}
-last_spoken = numbers[-1]
-
-for turn in range(len(numbers) + 1, TURNS + 1):
-    # "turn - 1" is the last turn where last_spoken was spoken
-    # last_turns[last_spoken] is the turn before that (not updated yet to "turn - 1").
-    if last_spoken in last_turns:
-        spoken = turn - 1 - last_turns[last_spoken]
-    else:
-        spoken = 0
-    last_turns[last_spoken] = turn - 1
-    last_spoken = spoken
-
-print(last_spoken)
+print(memory_game(numbers, 2020))
+print(memory_game(numbers, 30_000_000))
