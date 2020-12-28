293e466d28dba7eb9215889e9e1a6dd343f227a7
Alex <alexprengere@gmail.com>
2020-12-28 18:07:35 +0100

    Clean up day 05

--- 2020/05/python/main.py
+++ 2020/05/python/main.py
@@ -1,25 +1,31 @@
 import sys
 
 
-seats = set()
-
-for line in sys.stdin:
-    line = line.strip()
-
+def compute_seat_id(data):
     row = 0
-    for cn, c in enumerate(line[:7]):
+    for cn, c in enumerate(data[:7]):
         if c == "B":
             row += 2 ** (6 - cn)
 
     col = 0
-    for cn, c in enumerate(line[7:]):
+    for cn, c in enumerate(data[7:]):
         if c == "R":
             col += 2 ** (2 - cn)
 
-    seat_id = 8 * row + col
-    seats.add(seat_id)
+    return row * 8 + col
 
 
-for i in range(max(seats) + 1):
+seats = set(compute_seat_id(line.rstrip()) for line in sys.stdin)
+
+
+# PART 1
+#
+print(max(seats))
+
+
+# PART 2
+#
+for i in reversed(range(max(seats) + 1)):
     if i not in seats:
         print(i)
+        break
