942a1de9fc72c7acfd1ece1073700fe02d2094a3
Alex <alexprengere@gmail.com>
2020-12-28 08:51:21 +0100

    Cleanup day 01

--- 2020/01/python/main.py
+++ 2020/01/python/main.py
@@ -3,11 +3,16 @@ import itertools
 import operator
 from functools import reduce
 
+
+def find_sum(data, N):
+    for numbers in itertools.combinations_with_replacement(data, N):
+        if sum(numbers) == 2020:
+            return reduce(operator.mul, numbers, 1)
+
+
 data = []
 for row in sys.stdin:
     data.append(int(row))
 
-for numbers in itertools.combinations_with_replacement(data, 3):
-    if sum(numbers) == 2020:
-        print(reduce(operator.mul, numbers, 1))
-        break
+print(find_sum(data, 2))
+print(find_sum(data, 3))
