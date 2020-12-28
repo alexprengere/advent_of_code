0ea02f986e13ab936b61702105d49bdd13baf32e
Alex <alexprengere@gmail.com>
2020-12-28 09:34:10 +0100

    Clean up day 09

--- 2020/09/python/main.py
+++ 2020/09/python/main.py
@@ -1,6 +1,27 @@
 import sys
 from itertools import islice
 
+PREAMBLE = 25
+
+
+def find_invalid_number(numbers, preamble=PREAMBLE):
+    for position, value in islice(enumerate(numbers), preamble, None):
+        prev_numbers = numbers[position - preamble : position]
+        sums = set(a + b for a in prev_numbers for b in prev_numbers if a != b)
+        if value not in sums:
+            return value
+
+
+def find_encryption_weakness(numbers, invalid_number):
+    for start_position, start_value in enumerate(numbers):
+        total = start_value
+        for position, value in enumerate(numbers[start_position + 1 :]):
+            total += value
+            if total == invalid_number:
+                values = numbers[start_position : start_position + position + 1]
+                return min(values) + max(values)
+
+
 numbers = []
 for row in sys.stdin:
     numbers.append(int(row))
@@ -8,22 +29,10 @@ for row in sys.stdin:
 
 # PART 1
 #
-PREAMBLE = 25
-for position, value in islice(enumerate(numbers), PREAMBLE, None):
-    prev_numbers = numbers[position - PREAMBLE : position]
-    sums = set(a + b for a in prev_numbers for b in prev_numbers if a != b)
-    if value not in sums:
-        N = value
-        print(N)
-        break
+invalid_number = find_invalid_number(numbers)
+print(invalid_number)
 
 
 # PART 2
 #
-for start_position, start_value in enumerate(numbers):
-    total = start_value
-    for position, value in enumerate(numbers[start_position + 1 :]):
-        total += value
-        if total == N:
-            values = numbers[start_position : start_position + position + 1]
-            print(min(values) + max(values))
+print(find_encryption_weakness(numbers, invalid_number))
