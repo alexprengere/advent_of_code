fecd3302398b189b5b29de783e32e026ba501943
Alex <alexprengere@gmail.com>
2020-12-28 19:36:39 +0100

    Clean up day 02

--- 2020/02/python/main.py
+++ 2020/02/python/main.py
@@ -1,10 +1,18 @@
 import sys
 
-total_valid = 0
-for row in sys.stdin:
-    range_, letter, password = row.strip().split()
-    first, last = range_.split("-", 1)
+rows = list(sys.stdin)
+
+total_valid_1 = 0
+total_valid_2 = 0
+
+for row in rows:
+    digits, letter, password = row.strip().split()
+    digits = [int(n) for n in digits.split("-", 1)]
     letter = letter.rstrip(":")
-    if sum(password[int(pos) - 1] == letter for pos in (first, last)) == 1:
-        total_valid += 1
-print(total_valid)
+    if digits[0] <= password.count(letter) <= digits[1]:
+        total_valid_1 += 1
+    if sum(password[n - 1] == letter for n in digits) == 1:
+        total_valid_2 += 1
+
+print(total_valid_1)
+print(total_valid_2)
