81a3dea55de795beb67e5084a327090b93f67ef8
Alex <alexprengere@gmail.com>
2020-12-28 09:13:07 +0100

    Cleanup day 07

--- 2020/07/python/main.py
+++ 2020/07/python/main.py
@@ -1,6 +1,35 @@
 import sys
 from collections import defaultdict
 
+
+def find_bags_containing(rules, searched_color):
+    solutions = set()
+    stack = [searched_color]
+    while stack:
+        target_color = stack.pop()
+        for color, inside in rules.items():
+            for _, col in inside:
+                if col == target_color:
+                    if color not in solutions:
+                        stack.append(color)
+                        solutions.add(color)
+    return len(solutions)
+
+
+def find_bags_inside(rules, root_color):
+    total = 0
+    stack = [root_color]
+    while stack:
+        color = stack.pop()
+        for n, col in rules[color]:
+            for _ in range(n):
+                total += 1
+                stack.append(col)
+    return total
+
+
+# READING INPUT
+#
 rules = defaultdict(list)
 
 for row in sys.stdin:
@@ -13,29 +42,9 @@ for row in sys.stdin:
 
 # PART 1
 #
-stack = [("shiny", "gold")]
-solutions = set()
-while stack:
-    target_color = stack.pop()
-    for color, inside in rules.items():
-        for _, col in inside:
-            if col == target_color:
-                if color not in solutions:
-                    stack.append(color)
-                    solutions.add(color)
-
-print(len(solutions))
+print(find_bags_containing(rules, ("shiny", "gold")))
 
 
 # PART 2
 #
-stack = [("shiny", "gold")]
-total = 0
-while stack:
-    color = stack.pop()
-    for n, col in rules[color]:
-        for _ in range(n):
-            total += 1
-            stack.append(col)
-
-print(total)
+print(find_bags_inside(rules, ("shiny", "gold")))
