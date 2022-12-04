2a7d6261126c8458610fdcd2c30147bb107e090b
Alex <alexprengere@gmail.com>
2022-12-04 17:49:14 +0100

    Add part 2

--- 2022/03/python/main.py
+++ 2022/03/python/main.py
@@ -7,11 +7,39 @@ def get_priority(item):
     return PRIORITIES.find(item) + 1
 
 
-sum_of_priorities = 0
-for row in sys.stdin:
-    items = row.rstrip()
-    comp_0 = items[: len(items) // 2]
-    comp_1 = items[len(items) // 2:]
-    common = (set(comp_0) & set(comp_1)).pop()
-    sum_of_priorities += get_priority(common)
-print(sum_of_priorities)
+def part1():
+    sum_of_priorities = 0
+    for row in sys.stdin:
+        items = row.rstrip()
+        comp_0 = items[: len(items) // 2]
+        comp_1 = items[len(items) // 2 :]
+        common = (set(comp_0) & set(comp_1)).pop()
+        sum_of_priorities += get_priority(common)
+    print(sum_of_priorities)
+
+
+# PART 2
+#
+from itertools import islice
+
+
+def batched(iterable, n):
+    "Batch data into lists of length n. The last batch may be shorter."
+    # batched('ABCDEFG', 3) --> ABC DEF G
+    it = iter(iterable)
+    while True:
+        batch = list(islice(it, n))
+        if not batch:
+            return
+        yield batch
+
+
+def part2():
+    sum_of_priorities = 0
+    for group in batched((row.rstrip() for row in sys.stdin), 3):
+        common = set.intersection(*(set(rs) for rs in group)).pop()
+        sum_of_priorities += get_priority(common)
+    print(sum_of_priorities)
+
+
+part2()
