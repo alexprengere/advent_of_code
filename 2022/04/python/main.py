9e064332119161e87c13d58b86c2b76e7f8ef62a
Alex <alexprengere@gmail.com>
2022-12-04 18:11:03 +0100

    Add part 2

--- 2022/04/python/main.py
+++ 2022/04/python/main.py
@@ -1,19 +1,20 @@
 import sys
 
+PART = 2
 
-def part1():
-    redundant = 0
-    for row in sys.stdin:
-        s0, s1 = row.rstrip().split(",")
-        s0 = s0.split("-")
-        s1 = s1.split("-")
-        sections_0_limits = int(s0[0]), int(s0[1]) + 1
-        sections_1_limits = int(s1[0]), int(s1[1]) + 1
-        sections_0 = set(range(*sections_0_limits))
-        sections_1 = set(range(*sections_1_limits))
+redundant = 0
+for row in sys.stdin:
+    s0, s1 = row.rstrip().split(",")
+    s0 = s0.split("-")
+    s1 = s1.split("-")
+    sections_0_limits = int(s0[0]), int(s0[1]) + 1
+    sections_1_limits = int(s1[0]), int(s1[1]) + 1
+    sections_0 = set(range(*sections_0_limits))
+    sections_1 = set(range(*sections_1_limits))
+    if PART == 1:
         if sections_0 <= sections_1 or sections_1 <= sections_0:
             redundant += 1
-    print(redundant)
-
-
-part1()
+    elif PART == 2:
+        if sections_0 & sections_1:
+            redundant += 1
+print(redundant)
