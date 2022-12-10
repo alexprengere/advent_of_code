3408d42c6ff0d17af30db114a213b2452910838d
Alex <alexprengere@gmail.com>
2022-12-10 18:28:02 +0100

    Add part 2

--- 2022/10/python/main.py
+++ 2022/10/python/main.py
@@ -17,8 +17,25 @@ def run():
             X += int(n)
 
 
+execution = list(run())
+
+
+# PART 1
+#
 total = 0
-for cycle, X in run():
+for cycle, X in execution:
     if cycle % 40 == 20:
         total += cycle * X
 print(total)
+
+
+# PART 2
+#
+for cycle, X in execution:
+    crt_position = (cycle - 1) % 40
+    if X - 1 <= crt_position <= X + 1:
+        print("#", end="")
+    else:
+        print(" ", end="")
+    if crt_position == 39:
+        print()
