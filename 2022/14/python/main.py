70f5de585436db709c57a5108f56fbc720775974
Alex <alexprengere@gmail.com>
2022-12-14 12:01:15 +0100

    Refactor

--- 2022/14/python/main.py
+++ 2022/14/python/main.py
@@ -2,16 +2,6 @@ import sys
 import itertools
 
 
-def show(grid):
-    x_min, *_, x_max = sorted(x for x, _ in grid)
-    y_min, *_, y_max = sorted(y for _, y in grid)
-    for y in range(y_min, y_max + 1):
-        print(y, end=" ")
-        for x in range(x_min, x_max + 1):
-            print(grid.get((x, y), "."), end="")
-        print()
-
-
 def straight_line(p0, p1):
     x1, y1 = p1
     xi, yi = p0
@@ -22,22 +12,29 @@ def straight_line(p0, p1):
     yield p1
 
 
-def get_sand_position(grid, source):
-    y_max = max(y for _, y in grid)
+class OverflowingSand(Exception):
+    pass
+
+
+def get_sand_position(grid, source, bottom, bottom_blocks=False):
     xs, ys = source
-    while ys <= y_max:
-        for dx, dy in [(0, 1), (-1, 1), (1, 1)]:
-            next_s = xs + dx, ys + dy
-            if grid.get(next_s) not in {"#", "o", "+"}:
-                xs, ys = next_s
+    while ys <= bottom:
+        for dx in (0, -1, 1):
+            next_xs = xs + dx
+            next_ys = ys + 1
+            value = grid.get((next_xs, next_ys), ".")
+            blocked = (next_ys == bottom and bottom_blocks) or value in "#o"
+            if not blocked:
+                xs, ys = next_xs, next_ys
                 break
         else:  # no break = sand is blocked
             return xs, ys
-    else:  # ys > y_max
-        return None
+    raise OverflowingSand()
 
 
-grid = {}
+SOURCE = (500, 0)
+grid = {SOURCE: "+"}
+
 for row in sys.stdin:
     for p0, p1 in itertools.pairwise(row.split("->")):
         x0, y0 = map(int, p0.split(","))
@@ -45,15 +42,30 @@ for row in sys.stdin:
         for p in straight_line((x0, y0), (x1, y1)):
             grid[p] = "#"
 
+y_max = max(y for _, y in grid)
 
-SOURCE = (500, 0)
-grid[SOURCE] = "+"
+for turn in itertools.count(1):
+    try:
+        sand = get_sand_position(
+            grid,
+            SOURCE,
+            bottom=y_max,
+            bottom_blocks=False,
+        )
+    except OverflowingSand:
+        print(turn - 1)  # -1 because we want the turn before overflow
+        break
+    else:
+        grid[sand] = "o"
 
-for turn in itertools.count():
-    sand = get_sand_position(grid, SOURCE)
-    if sand is None:
+for turn in itertools.count(turn):
+    sand = get_sand_position(
+        grid,
+        SOURCE,
+        bottom=y_max + 2,
+        bottom_blocks=True,
+    )
+    if sand == SOURCE:
+        print(turn)
         break
     grid[sand] = "o"
-
-# show(grid)
-print(turn)
