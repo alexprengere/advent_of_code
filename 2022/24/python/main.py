e539719eab39802d3afe80d1342d28546b3da124
Alex <alexprengere@gmail.com>
2022-12-30 13:43:38 +0100

    Clean up day 24

--- 2022/24/python/main.py
+++ 2022/24/python/main.py
@@ -1,19 +1,12 @@
 import sys
-import math
 import itertools
 from collections import defaultdict
 
-DIRECTIONS = {
-    "<": (-1, +0),
-    ">": (+1, +0),
-    "v": (+0, +1),
-    "^": (+0, -1),
-}
 
-
-def neighbors(x, y):
+def movements(x, y):
+    # Possible movements from a point (x, y)
     for dx, dy in [
-        (+0, +0),  # not moving
+        (+0, +0),  # waiting
         (+1, +0),
         (-1, +0),
         (+0, +1),
@@ -22,6 +15,43 @@ def neighbors(x, y):
         yield x + dx, y + dy
 
 
+def move(blizzards):
+    # Simulate blizzards next turn.
+    # Note that there is a period of math.lcm(X_MAX - 1, Y_MAX - 1)
+    # in blizzards pattern, but we do not use that.
+    updates = []
+
+    for (x, y) in blizzards:
+        for (dx, dy) in blizzards[x, y]:
+            nx = x + dx
+            ny = y + dy
+            if (nx, ny) in WALLS:
+                if dx == +1:
+                    nx = X_MIN + 1
+                elif dy == +1:
+                    ny = Y_MIN + 1
+                elif dx == -1:
+                    nx = X_MAX - 1
+                elif dy == -1:
+                    ny = Y_MAX - 1
+                else:
+                    raise AssertionError("Unknown direction")
+            updates.append(((x, y), (nx, ny), (dx, dy)))
+
+    # The order matters here, otherwise you might remove a new blizzard
+    # arriving at position p
+    for p, _, direction in updates:
+        blizzards[p].remove(direction)
+    for _, p, direction in updates:
+        blizzards[p].add(direction)
+
+
+DIRECTIONS = {
+    "<": (-1, +0),
+    ">": (+1, +0),
+    "v": (+0, +1),
+    "^": (+0, -1),
+}
 WALLS = set()
 blizzards = defaultdict(set)
 
@@ -34,75 +64,39 @@ for y, row in enumerate(_input.splitlines()):
         elif c in "<>v^":
             blizzards[point].add(DIRECTIONS[c])
 
-
 X_MIN = min(x for x, _ in WALLS)
 X_MAX = max(x for x, _ in WALLS)
 Y_MIN = min(y for _, y in WALLS)
 Y_MAX = max(y for _, y in WALLS)
 
+P0 = X_MIN + 1, Y_MIN
+P1 = X_MAX - 1, Y_MAX
 
-def move(blizzards):
-    # Simulate blizzards next turn.
-    # Note that there is a period of math.lcm(X_MAX - 1, Y_MAX - 1)
-    # in blizzards pattern, but we do not use that.
-    changes = []
+points, visited = {P0}, {P0: 0}
+path = [P1, P0, P1]
+target = path.pop()
 
-    for (x, y) in blizzards:
-        for (dx, dy) in blizzards[x, y]:
-            nx = x + dx
-            ny = y + dy
-            if (nx, ny) in WALLS:
-                if dx == 1:
-                    nx = 1
-                elif dy == 1:
-                    ny = 1
-                elif dx == -1:
-                    nx = X_MAX - 1
-                elif dy == -1:
-                    ny = Y_MAX - 1
-                else:
-                    raise AssertionError("Unknown direction")
-            changes.append(((x, y), (nx, ny), (dx, dy)))
+for time in itertools.count():
+    if target in visited:
+        print(f"Time to reach: {visited[target]}")
+        points, visited = {target}, {target: 0}
+        if not path:
+            break
+        target = path.pop()
 
-    for p, _, direction in changes:
-        blizzards[p].remove(direction)
-    for _, p, direction in changes:
-        blizzards[p].add(direction)
+    # Update the blizzards positions
+    move(blizzards)
 
-
-def advance(explored, blizzards):
-    # Move explored points to their neighbors based on latest blizzards.
-    for x, y in list(explored):
-        explored.remove((x, y))
-        for nx, ny in neighbors(x, y):
+    # Update points positions to their 'neighbors'
+    for x, y in list(points):
+        points.remove((x, y))
+        for nx, ny in movements(x, y):
             if (nx, ny) in WALLS:
                 continue
             if blizzards[nx, ny]:
                 continue
             if 0 <= nx <= X_MAX and 0 <= ny <= Y_MAX:
-                explored.add((nx, ny))
-
-
-P0 = X_MIN + 1, Y_MIN
-P1 = X_MAX - 1, Y_MAX
-
-explored = {P0}
-fastest = {}
-
-path = [P1, P0, P1]
-target = path.pop()
-
-for time in itertools.count():
-    if target in fastest:
-        print(fastest[target])
-        explored = {target}
-        fastest.clear()
-        if path:
-            target = path.pop()
-        else:
-            break
-
-    move(blizzards)
-    advance(explored, blizzards)
-    for p in explored:
-        fastest[p] = min(fastest.get(p, math.inf), time + 1)
+                points.add((nx, ny))
+                # points visiting a new coordinate are recorded in visited
+                if (nx, ny) not in visited:
+                    visited[nx, ny] = time + 1
