766176b486e286a198f8112b5287d2600a997a80
Alex <alexprengere@gmail.com>
2024-12-22 19:56:02 +0100

    Optimize day 22

--- 2024/22/python/main.py
+++ 2024/22/python/main.py
@@ -1,5 +1,5 @@
 import sys
-from collections import deque
+from collections import Counter
 
 secrets = [int(row) for row in sys.stdin]
 
@@ -14,70 +14,41 @@ def evolve(secret):
     return secret
 
 
-def evolve_multi(secret, times=1):
-    for _ in range(times):
-        secret = evolve(secret)
-    return secret
+# I used to represent the sequence of last 4 diffs using a deque(maxlen=4),
+# but as we know each of the 4 numbers is in the range [-9, 9], we can encode
+# them in 5 bits each: adding 9 gives the [0, 18], and 2**4 <= 18 < 2**5.
+# So our number looks like this: 0b00000_00000_00000_00000
+#                                  ^^^^^             ^^^^^
+#                               newest diff       oldest diff
+# The initial value is 11111_11111_11111_11111, and note that as 11111 does
+# not represent a value in [0, 18], we can test this to know if we do not
+# have the 4 diffs yet (by masking with 0b11111).
+# Using this representation yields impressive results on PyPy, not so much on CPython.
 
+evolved_secrets_sum = 0  # for part 1
+price_per_seq: dict[int, int] = Counter()  # for part 2
+seq_seen_for_secret = set()
 
-# PART 1
-#
-N = 2000
-print(sum(evolve_multi(secret, times=N) for secret in secrets))
-
-
-# PART 2
-#
-def show(secret, times):
-    price = secret % 10
-    print(f"{secret:>15d}: {price}")
-
-    for _ in range(times - 1):
-        secret = evolve(secret)
-        last_price, price = price, secret % 10
-        diff = price - last_price if last_price is not None else 0
-        print(f"{secret:>15d}: {price} ({diff:+>2d})")
-
-
-def get_seqs_until(secret, times):
-    last_diffs = deque(maxlen=4)
-    price = secret % 10
-    for _ in range(times):
-        secret = evolve(secret)
-        last_price, price = price, secret % 10
-        if last_price is not None:
-            last_diffs.append(price - last_price)
-        if len(last_diffs) == 4:
-            yield (price, tuple(last_diffs))
-
-
-show(123, 10)
-
-
-CACHE = {}
 for secret in secrets:
-    seq_to_price = {}
-    for price, seq in get_seqs_until(secret, times=N):
-        if seq not in seq_to_price:
-            seq_to_price[seq] = price
-    CACHE[secret] = seq_to_price
+    seq_seen_for_secret.clear()
 
+    seq = (1 << 20) - 1
+    last_price = secret % 10
 
-all_seq = set()
-for secret in secrets:
-    all_seq.update(CACHE[secret])
+    for _ in range(2000):
+        secret = evolve(secret)
+        price = secret % 10
+        # Updating seq by right shifting by 5 bits to remove the oldest diff,
+        # and adding the newest diff in the leftmost 5 bits.
+        seq >>= 5
+        seq |= (price - last_price + 9) << 15
 
+        if seq not in seq_seen_for_secret and seq & 0b11111 != 0b11111:
+            seq_seen_for_secret.add(seq)
+            price_per_seq[seq] += price
 
-print(len(all_seq))
+        last_price = price
+    evolved_secrets_sum += secret
 
-
-max_price = 0
-max_seq = None
-for i, seq in enumerate(all_seq):
-    if i % 1000 == 0:
-        print(i, len(all_seq))
-    price = sum(CACHE[secret].get(seq, 0) for secret in secrets)
-    if price > max_price:
-        max_price = price
-        max_seq = seq
-print(max_price, max_seq)
+print(evolved_secrets_sum)
+print(max(price_per_seq.values()))
