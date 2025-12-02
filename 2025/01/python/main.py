aef305f2695b9a08f86fd971a5eb18a05f081f8b
Alex <alexprengere@gmail.com>
2025-12-02 10:14:47 +0100

    Cleanup day 01

--- 2025/01/python/main.py
+++ 2025/01/python/main.py
@@ -1,33 +1,38 @@
 import sys
 
 
-def count_zeros(start, stop):
-    # Dividing by 100 to count how many times we crossed a multiple of 100,
-    # and adjusting for direction of movement: we add -1 when moving left to
-    # adjust when we start exactly on zero.
-    if stop > start:
-        return stop // 100 - start // 100
-    else:
-        return (start - 1) // 100 - (stop - 1) // 100
+def move(start, shift):
+    crosses, dial = divmod(start + shift, 100)
+    crosses = abs(crosses)
+    if shift < 0:
+        # The negative direction require some adjustments
+        # * starting at 0 and going negative does not count as crossing zero
+        # * ending at 0 is not already counted as crossing zero, so we add 1
+        if start == 0:
+            crosses -= 1
+        if dial == 0:
+            crosses += 1
+    return crosses, dial
 
 
 _input = sys.stdin.readlines()
 
+DIRECTIONS = {"R": 1, "L": -1}
 points_at_zero = 0
-clicks_at_zero = 0
+crosses_zero = 0
 dial = 50
 
 for row in _input:
-    r, n = row[0], int(row[1:])
-    if r == "R":
-        new_dial = dial + n
-    elif r == "L":
-        new_dial = dial - n
+    direction, n = row[0], int(row[1:])
+    if direction == "R":
+        shift = n
+    elif direction == "L":
+        shift = -n
 
-    clicks_at_zero += count_zeros(dial, new_dial)
-    dial = new_dial % 100
+    crosses, dial = move(dial, n * DIRECTIONS[direction])
+    crosses_zero += crosses
     if dial == 0:
         points_at_zero += 1
 
 print(points_at_zero)
-print(clicks_at_zero)
+print(crosses_zero)
