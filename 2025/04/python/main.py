b17da7372f1f9a19b0e8014d1a17ae1f283d97a2
Alex <alexprengere@gmail.com>
2025-12-04 15:06:36 +0100

    Add day 04 part 2

--- 2025/04/python/main.py
+++ 2025/04/python/main.py
@@ -1,44 +1,61 @@
 import sys
 import itertools
 
-
-D_INDICES = set(itertools.product((-1, 0, 1), repeat=2))
-
-
-def neighbors(x, y):
-    for dx, dy in D_INDICES:
-        if (dx, dy) != (0, 0):
-            yield (x + dx, y + dy)
-
-
 _input = sys.stdin.readlines()
 ROWS = len(_input)
 COLS = len(_input[0].rstrip())
 
-grid = set()
-for y, row in enumerate(_input):
-    for x, value in enumerate(row.rstrip()):
-        if value == "@":
-            grid.add((x, y))
 
-
-def draw_grid(grid, reachable):
+def draw(rolls):
     for y in range(ROWS):
         for x in range(COLS):
-            if (x, y) in reachable:
-                print("x", end="")
-
-            elif (x, y) in grid:
-                print("@", end="")
+            if (x, y) in rolls:
+                print(len(rolls[x, y]), end="")
             else:
                 print(".", end="")
         print()
 
 
-reachable = set()
-for point in grid:
-    total = sum(neigh in grid for neigh in neighbors(*point))
-    if total < 4:
-        reachable.add(point)
+D_INDICES = set(itertools.product((-1, 0, 1), repeat=2)) - {(0, 0)}
 
-print(len(reachable))
+
+def neighbors(x, y):
+    for dx, dy in D_INDICES:
+        yield x + dx, y + dy
+
+
+# rolls is the mapping of {roll: {neighbor rolls}}, this is what is mutated.
+# Basically at each step, when a roll is removed, it "tells its neighbors",
+# then is actually removed from rolls. Only such neighbors are considered
+# for removal at the next step.
+# This way, it avoids re-counting rolls in neighbors at each step.
+# And we iterate until rolls is stable.
+rolls = {}
+for y, row in enumerate(_input):
+    for x, value in enumerate(row.rstrip()):
+        if value == "@":
+            rolls[x, y] = set()
+
+for point in rolls:
+    for neighbor in neighbors(*point):
+        if neighbor in rolls:
+            rolls[point].add(neighbor)
+
+nb_rolls_at_start = len(rolls)
+to_remove = [p for p in rolls if len(rolls[p]) < 4]
+candidates = set()
+print(len(to_remove))  # part 1
+
+while to_remove:
+    for point in to_remove:
+        for neighbor in rolls[point]:
+            rolls[neighbor].remove(point)
+            candidates.add(neighbor)
+        del rolls[point]
+    to_remove[:] = [p for p in candidates if p in rolls and len(rolls[p]) < 4]
+    candidates.clear()
+    # import os
+    # os.system('clear')
+    # draw(rolls)
+
+print(nb_rolls_at_start - len(rolls))  # part 2
